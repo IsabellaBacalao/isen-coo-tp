26f1690093d1ba93e3f10df29ed02b930ece1b7b
Baptiste Herbecq <baptiste.herbecq@gmail.com>
2023-10-20 17:07:25 +0200

    feat: Adding addTastedBeer

--- src/application/use-case/addTastedBeer.ts
+++ src/application/use-case/addTastedBeer.ts
@@ -1,9 +1,11 @@
+import { TastedBeer } from "../../domain/entity/TastedBeer";
 import { TastedBeerRepository } from "../../domain/repository/tastedBeerRepository";
 
-export type GetAllTastedBeersUseCaseDependencies = {
+export type addTastedBeersDependencies = {
   beerRepository: TastedBeerRepository;
+  tastedBeer: TastedBeer;
 };
 
-export const getAllTastedBeerUseCase = async (deps: GetAllTastedBeersUseCaseDependencies) => {
-  return await deps.beerRepository.getAllTastedBeers();
+export const addTastedBeer = async ({ beerRepository, tastedBeer }: addTastedBeersDependencies) => {
+  return await beerRepository.addTastedBeer(tastedBeer);
 };
