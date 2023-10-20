87790c81d95335c2a20bfe4d3cb9608c71c01242
leooz <leo59coc@gmail.com>
2023-10-20 14:00:38 +0200

    feat: a lot

--- src/domain/repository/tastedBeerRepository.ts
+++ src/domain/repository/tastedBeerRepository.ts
@@ -1,7 +1,7 @@
 import { BeerTasted } from "../entity/BeerTasted";
 
 export interface TastedBeerRepository {
-  getAllTastedBeer(): Promise<BeerTasted[]>;
-  addTastedBeer(beer: BeerTasted): Promise<void>;
+  getAllTastedBeers(): Promise<BeerTasted[]>;
+  addTastedBeers(beer: BeerTasted): Promise<void>;
   setBeerLikedOpinionOnTastedBeer(id: number, hasLiked: boolean): Promise<void>;
 }
---
+++ src/infrastructure/provider/punkApi/localTastedBeerRepository.ts
@@ -0,0 +1,67 @@
+import { promises } from "fs";
+import { join } from "path";
+import { BeerTasted } from "../../../domain/entity/BeerTasted";
+import { TastedBeerRepository } from "../../../domain/repository/tastedBeerRepository";
+
+export class LocalTastedBeerRepository implements TastedBeerRepository {
+  private filePath: string;
+
+  constructor() {
+    this.filePath = join(__dirname, "../../../data/prefered-beer.json");
+  }
+  addTastedBeers(beer: BeerTasted): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async getAllTastedBeers(): Promise<BeerTasted[]> {
+    try {
+      const data = await promises.readFile(this.filePath);
+
+      return JSON.parse(data.toString()).tastedBeers;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
+  }
+
+  async addTastedBeer(tastedBeer: BeerTasted): Promise<void> {
+    const tastedBeers = await this.getAllTastedBeers();
+
+    const hasAlreadyTasted = !!tastedBeers.find(({ id }) => id === tastedBeer.id);
+
+    if (hasAlreadyTasted) {
+      return;
+    }
+
+    tastedBeers.push(tastedBeer);
+
+    await promises.writeFile(
+      this.filePath,
+      JSON.stringify({
+        tastedBeers,
+      }),
+    );
+  }
+
+  async setBeerLikedOpinionOnTastedBeer(id: number, hasLiked: boolean): Promise<void> {
+    const tastedBeers = await this.getAllTastedBeers();
+    const tastedBeer = tastedBeers.find((tastedBeer) => tastedBeer.id === id);
+
+    if (!tastedBeer) {
+      throw new Error("Not found");
+    }
+
+    const indexOfTastedBeer = tastedBeers.map(({ id }) => id).indexOf(id);
+
+    tastedBeer.hasLiked = hasLiked;
+
+    tastedBeers[indexOfTastedBeer] = tastedBeer;
+
+    await promises.writeFile(
+      this.filePath,
+      JSON.stringify({
+        tastedBeers,
+      }),
+    );
+  }
+}
